ed41a851fa8eb703d634adc3deb21905a30f9dbf
Prebbles <paula.vandenberg@student.hu.nl>
2023-02-05 20:59:47 +0100

    first commit

---
+++ ConsoleApp_met_Documentatie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_met_Documentatie
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
